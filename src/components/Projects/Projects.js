bf9339f109b0bf2468e22dc8e3aadffa9ae05095
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-05-23 19:08:05 +0530

    Add new assets: logo and loading animation

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import emotion from "../../Assets/Projects/logo/image.png";
-import editor from "../../Assets/Projects/logo/TaskTracker.png";
-import chatify from "../../Assets/Projects/logo/News.jpg";
-import suicide from "../../Assets/Projects/logo/ExApp.png";
-import bitsOfCode from "../../Assets/Projects/logo/Student.png";
+import ThoughtFlow from "../../Assets/ProjectLogos/image.png";
+import TaskTracker from "../../Assets/ProjectLogos/TaskTracker.png";
+import NewsNow from "../../Assets/ProjectLogos/News.jpg";
+import ExApp from "../../Assets/ProjectLogos/ExApp.png";
+import Student from "../../Assets/ProjectLogos/Student.png";
 
 function Projects() {
   return (
@@ -22,7 +22,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={NewsNow}
               isBlog={false}
               title="News Now"
               description="Application that provides the latest news articles Stay updated with News Now is a beautifully designed mobile application built using Flutter , offering seamless access to real-time news from across the world, powered by Supabase for authentication. With integration to the powerful News API , this app delivers curated news articles from various categories such as technology, sports, business, and more."
@@ -32,42 +32,42 @@ function Projects() {
 
           <Col md={5} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={ThoughtFlow}
               isBlog={false}
               title="ThoughFlow"
               description="ThoughFlow is a modern blogging platform built with Flutter for the frontend, Node.js and Express.js for the backend, and Supabase for authentication and database management."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/UKbhatt/ThoughtFlow"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={TaskTracker}
               isBlog={false}
               title="Task Tracker"
               description="A sleek and simple Task Tracker app built with Flutter and Supabase — track, manage, and organize your tasks with ease. Features task creation with due dates, completion toggles, and a stylish glassmorphic interface."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/UKbhatt/Task_Tracker"
                          
             />
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={ExApp}
               isBlog={false}
               title="Text Extractor Application"
               description="A sleek and simple Task Tracker app built with Flutter and Supabase — track, manage, and organize your tasks with ease. Features task creation with due dates, completion toggles, and a stylish glassmorphic interface."
-              ghLink="https://github.com/UKbhatt/Text_Extractor_Application"
+              ghLink="https://github.com/UKbhatt/Text_Extractor_App"
                          
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Student}
               isBlog={false}
               title="Student Management System"
               description="The College Management System (CMS) is a comprehensive solution designed to enhance the management of educational institutions. It streamlines various operations, including the management of departments, instructors, courses, students, and enrollments. By employing role-based access, the system ensures secure and organized operations for administrators, instructors, and students alike. Built using PHP and MySQL, the CMS offers robust server-side logic and efficient data storage capabilities"
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              ghLink="https://github.com/UKbhatt/Student_Management_System"
             />
           </Col>
         </Row>
